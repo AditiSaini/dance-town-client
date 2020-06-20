199e426cea3ea3b27f5ccf27654e37160b97adbb
Chien Dat Nguyen Dinh <chiendat@Mac-Dat.local>
2020-06-20 06:44:10 -0300

    Change Tag Name

--- src/containers/Challenges.js
+++ src/containers/Challenges.js
@@ -145,7 +145,6 @@ export default function Notes() {
                     {challenge.attachment && (
                         <video width="320" height="240" controls>
                             <source src={challenge.attachmentURL} type="video/mp4" />
-                            Your browser does not support the video tag.
                         </video>
                     )}
                     <FormGroup controlId="file">
