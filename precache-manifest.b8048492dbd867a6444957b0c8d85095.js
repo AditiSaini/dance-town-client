13272f304e999a85f98f6cf4407febb6559bc31a
Chien Dat Nguyen Dinh <chiendat@Mac-Dat.local>
2020-06-20 06:14:57 -0300

    Updates

---
+++ precache-manifest.b8048492dbd867a6444957b0c8d85095.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9ee82a0c302a93ae8fb95ab118fcd4d",
+    "url": "/dance-town-client/index.html"
+  },
+  {
+    "revision": "eb8ef4e66561231d21b7",
+    "url": "/dance-town-client/static/css/main.541a3fdf.chunk.css"
+  },
+  {
+    "revision": "b46ef0568036cdad864e",
+    "url": "/dance-town-client/static/js/2.863ea805.chunk.js"
+  },
+  {
+    "revision": "2c69aaf6155d411bfd5915efbd3f1059",
+    "url": "/dance-town-client/static/js/2.863ea805.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb8ef4e66561231d21b7",
+    "url": "/dance-town-client/static/js/main.f582e475.chunk.js"
+  },
+  {
+    "revision": "92327b3b52e2d34aef85",
+    "url": "/dance-town-client/static/js/runtime-main.078f421a.js"
+  }
+]);
