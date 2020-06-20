3fb964dd3efd5100e8cd5d00d06b2a4ea932be1f
Chien Dat Nguyen Dinh <chiendat@Mac-Dat.local>
2020-06-20 06:04:06 -0300

    Skeleton for Web App

---
+++ build/precache-manifest.b2e81b9fbdefe6a619695269860d664f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7e8010b2eac7693a47b9a0f3a96675d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12916a00908075ed665b",
+    "url": "/static/css/main.c0596ce7.chunk.css"
+  },
+  {
+    "revision": "b46ef0568036cdad864e",
+    "url": "/static/js/2.863ea805.chunk.js"
+  },
+  {
+    "revision": "2c69aaf6155d411bfd5915efbd3f1059",
+    "url": "/static/js/2.863ea805.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12916a00908075ed665b",
+    "url": "/static/js/main.578c0cd1.chunk.js"
+  },
+  {
+    "revision": "50c7e57897fe32ba4108",
+    "url": "/static/js/runtime-main.9d071911.js"
+  }
+]);
